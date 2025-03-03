6860a8a57d5d728268c1b541fd904315e5b4c95a
FahriHusain <fahrihusain243@gmail.com>
2025-03-04 05:18:45 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// slide foto
+let currentSlide = 0;
+
+function changeSlide(direction) {
+  const slides = document.querySelector(".slides");
+  const totalSlides = slides.children.length;
+
+  currentSlide += direction;
+  if (currentSlide < 0) {
+    currentSlide = totalSlides - 1;
+  } else if (currentSlide >= totalSlides) {
+    currentSlide = 0;
+  }
+
+  slides.style.transform = `translateX(${-currentSlide * 100}%)`;
+}
